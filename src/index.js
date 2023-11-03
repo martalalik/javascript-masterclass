734ef0b54afeaa3f88fc81a0c152b4edace7640f
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-11-03 09:59:01 +0100

    FP - Lambda Expressions vs Anonymous Functions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import '../assets/css/style.css';
 // Functional Programming
 // --------------------------------------------------------------
 
-// Imperative vs Declarative Programming
+// Lambda Expressions vs Anonymous Functions
 
-// - Technically things are imperative from the get go. Previously in JS we would only write imperative style code.
+// - In JS functions can be used as data, because they are also first-class citizens, which means that they can also DECLARED as a VALUE!!! Such as being ASSIGNED to a VARIABLE!!!
+// - EXPRESSION -> is assigned to a variable.
 
 const items = Object.freeze([
   { id: '🍔', name: 'Super Burger', price: 399 },
@@ -15,21 +16,31 @@ const items = Object.freeze([
 ]);
 console.log(items); // (3)[{…}, {…}, {…}]
 
-// IMPERATIVE - it is a LIST of INSTRUCTIONS how to achieve it. The end result doesn't matter. It's the STYLE of the CODE that we write. Describing WHAT.
-const itemNamesImperative = [];
-for (let i = 0; i< items.length; i++) { // iterating indexes
-  const item = items[i]; // calculating how to access each item
-  itemNamesImperative.push(item.name);
+// NORMAL FUNCTION / FUNCTION DECLARATION
+function getItemName(item) {
+  return item.name
 }
-console.log(itemNamesImperative); // ['Super Burger', 'Jumbo Fries', 'Big Slurp']
+console.log(items.map(getItemName)); // ['Super Burger', 'Jumbo Fries', 'Big Slurp'] -> passing getItemName function in as a value!!!
 
-// DECLARATIVE - programing with declaration via sentences. Describing HOW.
-// with map() - described what is happening it doesn't mutate any state.
-// CHAINING technique - is when prototype methods are changing.
-// Here we're getting a new array.
-const itemTotalDeclarative = items
-    .map((item) => item.price)
-    .reduce((price, nextPrice) => price + nextPrice); // chaining
-console.log(itemTotalDeclarative); // 897
+// LAMBDA EXPRESSIONS
+// - get bound to a variable
+// - we are passing the value of the function to be used as data elsewhere in the app. We are not calling getItemNameExp function directly, we are simply passing it as an argument into another function.
+// - Typically we will use lambda functions/expressions to compose more complex functions in functional programing paradigm.
+const getItemNameExp = (item) => item.name;
+console.log(items.map(getItemNameExp)); // ['Super Burger', 'Jumbo Fries', 'Big Slurp'] -> getItemNameExp function passed as an argument.
 
-console.log(items); // (3)[{…}, {…}, {…}]
+// FUNCTION EXPRESSION -> by assigning getItemName function as a value.
+// const getItemNameExpression = function getItemName(item) {
+//   return item.name
+// }
+
+// FUNCTION CALLBACK -> directly called function
+// + ANONYMOUS FUNCTION ->
+// - function doesn't have a name,
+// cannot be accessed after it is created, we cannot reference this function variable anywhere.
+// console.log(items.map(function (item) {
+//   return item.name
+// }));
+
+
+
