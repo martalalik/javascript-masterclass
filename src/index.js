c874559dc3c9f9fe4f3bfb9574a355becf519ca7
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-11-06 11:53:38 +0100

    FP - Higher-Order Functions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import '../assets/css/style.css';
 // Functional Programming
 // --------------------------------------------------------------
 
-// Function Closures
+// Higher-Order Functions HOF
 
-// - A closure is created with each new function that is declared.
-// - Closure gives access to the outer function scope even after outer function has return.
+// - HOF has 2 characteristics
+// - 1. RETURNS new FUNCTION -> (items) => items.find((item) => item.id === id).name;
+// - 2. Is a function that can take other functions as an arguments -> find()
+
+// - Functions in JS are FIRST-CLASS CITIZENS -> it's a VALUE, which means it can be assigned to a variable, or it can be passed as a value.
+// - PASSING as a value -> (item) => item.id === id
+// - ASSIGNING as a value -> (id) => (items) => items.find((item) => item.id === id).name to a variable -> const getNameFromId
+
+// BENEFITS of using HOF
+// - allows writing more simple and readable code.
+// - it is a nice abstraction that can be used through composition -> when we want to compose more complex software.
 
 const items = Object.freeze([
   { id: '🍔', name: 'Super Burger', price: 399 },
@@ -16,36 +25,12 @@ const items = Object.freeze([
 ]);
 console.log(items); // (3)[{…}, {…}, {…}]
 
-// closure 1
-// even we have returned, lifecycle of getNameFromId function is still alive in the program, which mean we have access to id in further functions calls
-const getNameFromId = (id) => {
-  // console.log(id); // 🍟
-  // when: return () => {} -> getNameFromId function is going to return, but we are not doing anything, so the return value of the function call here -> getNameFromId('🍟'); gives a new function!!!
-  // closure 2
-  return (items) => { // returning a new function
-    // in closure2, we have access to all variables from closure1.
-    // console.log(id, items); //  🍟 (3)[{…}, {…}, {…}]
-    return items.find((item) => item.id === id).name; // .name we want to print only name
-  } // this function here is exactly what we have available to us, with getFries.
-}
+// This is often called: CALLBACK FUNCTION, is because it is called back by the HOF. So the find() is in charge of invoking this; we are just supply it.
+const getNameFromId = (id) => (items) => items.find((item) => item.id === id).name;
 
-// getNameFromId('🍟');
-// now create a new const
-const getFries = getNameFromId('🍟'); // call getNameFromId function
-// console.log(getFries); // ƒ () {}
-// because is a function, we can call ia and pass in items.
-// console.log(getFries(items)); // when: return () => {} -> undefined - there is no response form getFries, because we are not returning anything -> return () => {} -> empty return // calling return () => {} function here
-// console.log(getFries(items)); // when: return (items) => {return items.find((item) => item.id === id) -> {id: '🍟', name: 'Jumbo Fries', price: 199}
-console.log(getFries(items)); // when: return (items) => {return items.find((item) => item.id === id).name -> Jumbo Fries
+const getFries = getNameFromId('🍟');
+const getBurger = getNameFromId('🍔');
 
-// HIGHER ORDER FUNCTION
-// - getNameFromId is acting as higher order function because it returns a new function.
-// - we could now reuse getFries function and passing different arrays. This is how pure functions and closure work.
-
-// just to show how it looks without description and refactored to lambda expression.
-const getNameFromIdCopy = (id) => (items) => items.find((item) => item.id === id).name; // getting oneliner and proper lambda expression. We call it lambda expression because it doesn't have a name, which means things are created anonymously, and we have an expression within a function closure.
-// reusability of getNameFromIdCopy function
-const getBurger = getNameFromIdCopy('🍔');
-const getSlurp = getNameFromIdCopy('🥤');
+console.log(getFries(items)); // Jumbo Fries
 console.log(getBurger(items)); // Super Burger
-console.log(getSlurp(items)); // Big Slurp
+
